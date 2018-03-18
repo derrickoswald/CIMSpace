9e98b1bd74351f4fa32abec9d3c3fd31cb648364
Derrick Oswald <derrick.oswald@gmail.com>
2018-03-18 19:19:28 +0100

    multiple house connections

--- js/makers/houseservicemaker.js
+++ js/makers/houseservicemaker.js
@@ -5,7 +5,7 @@
 
 define
 (
-    ["mustache", "cim", "./locationmaker", "./conductingequipmentmaker", "model/Core", "model/StateVariables"],
+    ["mustache", "cim", "./locationmaker", "./conductingequipmentmaker", "cancelablepromise", "model/Core", "model/StateVariables"],
     /**
      * @summary Make an EnergyConsumer CIM object representing a house service.
      * @description Digitizes a point and then a conductor with connectivity.
@@ -13,7 +13,7 @@ define
      * @exports houseservicemaker
      * @version 1.0
      */
-    function (mustache, cim, LocationMaker, ConductingEquipmentMaker, Core, StateVariables)
+    function (mustache, cim, LocationMaker, ConductingEquipmentMaker, CancelablePromise, Core, StateVariables)
     {
         class HouseServiceMaker extends ConductingEquipmentMaker
         {
@@ -26,6 +26,20 @@ define
             {
                 var template =
                 `
+                    <div class='form-group row'>
+                      <label class='col-sm-4 col-form-label' for='iterations'>Iterations</label>
+                      <div class='col-sm-8'>
+                        <input id='iterations' class='form-control' type='text' name='iterations' aria-describedby='iterationsHelp' value='3'>
+                        <small id='iterationsHelp' class='form-text text-muted'>Number of house services to create.</small>
+                      </div>
+                    </div>
+                    <div class='form-group row'>
+                      <label class='col-sm-4 col-form-label' for='mRID'>mRID</label>
+                      <div class='col-sm-8'>
+                        <input id='mRID' class='form-control' type='text' name='mRID' aria-describedby='mRIDHelp' value='{{mRID}}'>
+                        <small id='mRIDHelp' class='form-text text-muted'>Unique identifier (or initial identifier) for house services.</small>
+                      </div>
+                    </div>
                     <div class='form-group row'>
                       <label class='col-sm-4 col-form-label' for='customerCount'>Customer count</label>
                       <div class='col-sm-8'>
@@ -61,7 +75,7 @@ define
                     </div>
                 `;
                 var data = this._cimmap.get_data ();
-                var view = {};
+                var view = { mRID: this._cimedit.uuidv4 ()};
                 if (data)
                 {
                     var wireinfos = [];
@@ -95,7 +109,10 @@ define
 
             submit_parameters ()
             {
-                var consumer = { cls: "EnergyConsumer", EditDisposition: "new" };
+                var id = document.getElementById ("mRID").value;
+                if (!/([^\d]+)(\d+)$/.test (id))
+                    id = "HAS0001";
+                var consumer = { id: id, mRID: id, cls: "EnergyConsumer", EditDisposition: "new" };
                 var customerCount = document.getElementById ("customerCount").value;
                 if ("" != customerCount)
                     consumer.customerCount = customerCount;
@@ -125,18 +142,69 @@ define
                 return ([consumer, cable]);
             }
 
+            next (id)
+            {
+                function pad (width, string, padding)
+                {
+                    return ((width <= string.length) ? string : pad (width, padding + string, padding));
+                }
+                var ret = id + "0";
+                var regexp = /(\S+[^\d])(\d+)$/;
+                var array = regexp.exec (id);
+                if (array)
+                    ret = array[1] + pad (array[2].length, (Number (array[2]) + 1).toString (), "0");
+                return (ret);
+            }
+
             make ()
             {
                 var parameters = this.submit_parameters ();
+                var iterations = Number (document.getElementById ("iterations").value);
                 var consumer = parameters[0];
                 var cable = parameters[1];
-                consumer.id = this._cimedit.uuidv4 ();
                 var obj = this._cimedit.create_from (consumer);
-                var cpromise = this._digitizer.point (obj, this._cimedit.new_features ());
                 var lm = new LocationMaker (this._cimmap, this._cimedit, this._digitizer);
-                cpromise.setPromise (lm.make (cpromise.promise (), "wgs84"));
-                cpromise.setPromise (cpromise.promise ().then (this.make_equipment.bind (this)));
-                return (cpromise);
+                // set up iterations
+                var objs = [];
+                var id = obj.id;
+                for (var i = 0; i < iterations; i++)
+                {
+                    obj = JSON.parse (JSON.stringify (obj));
+                    obj.id = id;
+                    obj.mRID = id;
+                    objs.push (obj);
+                    id = this.next (id);
+                }
+
+                function do_one (obj)
+                {
+                    var cpromise = this._digitizer.point (obj, this._cimedit.new_features (), "<h1>Digitize point for " + obj.id + "</h1>");
+                    cpromise.setPromise (lm.make (cpromise.promise (), "wgs84").then (this.make_equipment.bind (this)));
+                    return (cpromise);
+                }
+
+                function joinem (obj, list_so_far)
+                {
+                    // update the screen
+                    this._cimedit.editnew (list_so_far);
+                    this._cimedit.edit (obj, true, true);
+                    // digitize another one and tack its results onto the result array
+                    var cpromise = do_one.call (this, obj);
+                    function append (list)
+                    {
+                        return (list_so_far.concat (list));
+                    }
+                    function catch_cancel (message)
+                    {
+                        console.log ("catch cancel " + message);
+                        return (list_so_far);
+                    }
+                    return (cpromise.promise ().then (append, catch_cancel));
+                }
+                var ret = do_one.call (this, objs[0]);
+                for (var i = 1; i < objs.length; i++)
+                    ret.setPromise (ret.promise ().then (joinem.bind (this, objs[i])));
+                return (ret);
             }
         }
 
