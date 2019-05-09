d07889b83bbf8b8834f06e14b2006a2e4642cb0f
Derrick Oswald <derrick.oswald@9code.ch>
2019-05-09 20:05:09 +0200

    Remove kludgy save code and replace with a hidden <a> and .click()

--- js/main.js
+++ js/main.js
@@ -16,13 +16,8 @@ requirejs
         // initialize widgets
         document.getElementById ("file_button").onchange = cimspace.file_change;
         document.getElementById ("connect").onclick = cimspace.process_url;
-        document.getElementById ("save_name").onchange = cimspace.save_name_change;
-        document.getElementById ("rdf_about").onchange = cimspace.about_change;
-        document.getElementById ("md_description").onchange = cimspace.description_change;
-        document.getElementById ("full_model").onchange = cimspace.save_mode_change;
-        document.getElementById ("difference_model").onchange = cimspace.save_mode_change;
-        document.getElementById ("only_new").onchange = cimspace.save_mode_change;
-        document.getElementById ("save_file").onclick = cimspace.generate_rdf;
+        document.getElementById ("save_file").onclick = cimspace.initialize_save_dialog;
+        document.getElementById ("save").onclick = cimspace.save;
         document.getElementById ("internal_features").onchange = cimmap.redraw;
         document.getElementById ("buildings_3d").onchange = cimmap.buildings_3d;
         document.getElementById ("scale_bar").onchange = cimmap.scale_bar;
