a6be526c0ae38719c9828cadfc7c5ef6f2bab1fc
Derrick Oswald <derrick.oswald@9code.ch>
2019-11-02 18:27:24 +0100

    remove 'self' references

--- js/cimmrid.js
+++ js/cimmrid.js
@@ -7,8 +7,8 @@ define
 (
     [],
     /**
-     * @summary Gnerate mRID values as best we can.
-     * @description Uses heuristics to create new mRID values following user freindly patterns,
+     * @summary Generate mRID values as best we can.
+     * @description Uses heuristics to create new mRID values following user friendly patterns,
      * and if that fails falls back to Universally Unique Identifiers:
      * http://www.ietf.org/rfc/rfc4122.txt
      * @exports CIMmrid
